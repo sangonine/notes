80fadd35f665dfbfe2564e844891b4957fd42d3d
Sangram Gondal <sangram.gondal@TEENSAFE.local>
2018-06-14 12:21:08 -0700

    Setup migration for notes and users table

--- database/migrations/2018_06_14_190417_create_notes_table.php
+++ database/migrations/2018_06_14_190417_create_notes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
-//            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title')->length(50);
+            $table->unsignedInteger('user_id');
+            $table->text('note')->length(1000);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notes');
     }
 }
